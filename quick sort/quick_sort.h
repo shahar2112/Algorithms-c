d1d7cdaa96b65cb7df01c5754aed3b064361beec
shahar2112 <65827115+shahar2112@users.noreply.github.com>
2020-09-09 15:27:44 +0300

    Delete quick_sort.h

--- quick sort/quick_sort.h
+++
@@ -1,9 +0,0 @@
-#ifndef __SIMPLE_SORTS_H__ 
-#define __SIMPLE_SORTS_H__
-
-#include <stddef.h> /*size_t */
-
-/*nmemb number of elements, size is element size */
-void QuickSort(void *base, size_t nmemb, size_t size, int(*compar)(const void *, const void *));
-
-#endif 
